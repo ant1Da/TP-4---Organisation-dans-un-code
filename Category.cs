26d489f109bde5f06501c84dca31dbe192864978
ant1Da <antoine.dast@edouardgand.fr>
2026-03-12 15:08:29 +0100

    Pas du tt fini

---
+++ Category.cs
@@ -0,0 +1,15 @@
+class Category
+{
+    private string name;
+    private string description;
+    public string Name
+    {
+        get {return name;}
+        set {name = value;}
+    }
+    public string Description
+    {
+        get {return description;}
+        set {description = value;}
+    }
+}

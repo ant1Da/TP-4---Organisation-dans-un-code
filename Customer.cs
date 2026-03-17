000449c782056c31036d7505d19f462b9cab48fa
ant1Da <antoine.dast@edouardgand.fr>
2026-03-17 08:51:33 +0100

    Test

---
+++ Customer.cs
@@ -0,0 +1,16 @@
+class Customer
+{
+    private string firstName;
+    private string lastName;
+    private string email;
+    public string FirstName
+    {
+        get {return firstName;}
+        set {firstName = value;}
+    }
+    public string LastName
+    {
+        get {return lastName;}
+        set {lastName = value;}
+    }
+}
